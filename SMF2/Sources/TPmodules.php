2819baa284266f1863d97580e2cf4496c5618a8c
IchBin <ichbin@ichbin.us>
2012-01-03 13:55:42 -0700

    Fixed verification error for comment captcha in articles.

--- SMF2/Sources/TPmodules.php
+++ SMF2/Sources/TPmodules.php
@@ -132,7 +132,7 @@ function TPmodules()
 			if ($_SESSION['visual_errors'] > 3 && isset($_SESSION['visual_verification_code']))
 				unset($_SESSION['visual_verification_code']);
 
-			fatal_lang_error('visual_verification_failed', false);
+			fatal_lang_error('error_wrong_verification_code', false);
 		}
 		elseif (isset($_SESSION['visual_errors']))
 			unset($_SESSION['visual_errors']);
