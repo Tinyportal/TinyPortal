6a1f624520aa8e2383a493f564bacfcf1d3b2174
illori <illori@simplemachines.org>
2015-12-04 16:19:49 -0500

    fix for article display.
    Signed-off-by: illori <illori@simplemachines.org>

--- Themes/default/TPsubs.template.php
+++ Themes/default/TPsubs.template.php
@@ -1854,7 +1854,7 @@ function template_blockarticle()
 }
 function blockarticle_renders()
 {
-	echo ' 
+	$code = '
 	<div class="blockarticle render1">
 		<div class="article_info">
 			{blockarticle_author} 
@@ -1865,7 +1865,9 @@ function blockarticle_renders()
 		<div class="article_padding">{blockarticle_moreauthor}</div>
 	</div>	
 		';
+	return $code;
 }
+ 
 
 function blockarticle_date($render = true)
 {
