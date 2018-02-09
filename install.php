95dcf19b5bf6d7172a91ee35fd6a3919ed9a669a
tinoest <tinoest@gmail.com>
2018-02-09 14:48:00 +0000

    Missed some versions

--- install.php
+++ install.php
@@ -46,7 +46,7 @@ if ($manual)
 	$render .= '
 	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 	<html xmlns="http://www.w3.org/1999/xhtml"><head>
-		<title>TinyPortal - v1.4R for SMF2.0.x</title>
+		<title>TinyPortal - v1.5.0 for SMF2.0.x</title>
 		 <link rel="stylesheet" type="text/css" href="'. $boardurl . '/Themes/default/css/index.css" />
 	</head><body>';
 
@@ -60,7 +60,7 @@ $render .= '<div id="hidemenow" style="z-index: 200; margin-bottom: 1em; positio
     document.getElementById("hidemenow").style.overflow = "hidden";	
     }
 </script>
-<div class="cat_bar" style="position:relative;"><a href="javascript:void(0)" style="position:absolute;top:5px;right:5px;font-weight:bold;color:red;" onclick="closeNav()"><img src="' . $boardurl . '/Themes/default/images/tinyportal/tp-delete_shout.gif" alt="*" /></a><h3 class="catbg">Install/Upgrade TinyPortal v1.4R for SMF 2.0.x<h3/></div>
+<div class="cat_bar" style="position:relative;"><a href="javascript:void(0)" style="position:absolute;top:5px;right:5px;font-weight:bold;color:red;" onclick="closeNav()"><img src="' . $boardurl . '/Themes/default/images/tinyportal/tp-delete_shout.gif" alt="*" /></a><h3 class="catbg">Install/Upgrade TinyPortal V1.5.0 for SMF 2.0.x<h3/></div>
 	<div class="middletext" style="padding: 2em; overflow: auto;">
 		<ul class="normallist" style="line-height: 1.7em;">';
 
@@ -671,7 +671,7 @@ if($smcFunc['db_num_rows']($request) > 0)
 else
 {
 	$newmod = array(
-		'version' => '1.4',
+		'version' => '1.5.0',
 		'modulename' => 'TPShout',	// must be exactly equal to the folder.
 		'title' => 'TP Simple Shout', 
 		'subquery' => 'shout',	// the subcall that let TP knows which module is running.
@@ -686,7 +686,7 @@ else
 		'blockrender' => 'tpshout_fetch',
 		'adminhook' => 'tpshout_adminhook',
 		'logo' => 'tpshoutbox.png',
-		'tpversion' => '1.4R',
+		'tpversion' => '1.5.0',
 		'smfversion' => '2.0.x',
 		'description' => '[b]TP Simple Shoutbox[/b] is the original shoutbox from v0.9 series of TinyPortal, now converted to a TP module. It allows shout in BBC format, scrolling of shouts, insert of BBC codes and smilies and an admin interface to delete or modify shouts.<br />	',
 		'author' => 'IchBin',
