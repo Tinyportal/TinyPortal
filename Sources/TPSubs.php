f30f16fad057cbe65fbf8dafd3f2b3116a392e08
tinoest <tinoest@gmail.com>
2018-02-11 18:34:20 +0000

    Move detect IE to just when wysiwyg is used to reduce load

--- Sources/TPSubs.php
+++ Sources/TPSubs.php
@@ -939,8 +939,60 @@ function TPwysiwyg_setup()
 		<script src="'.$boardurl.'/tp-files/tp-plugins/javascript/sceditor/minified/sceditor.min.js"></script>
 		<script src="'.$boardurl.'/tp-files/tp-plugins/javascript/sceditor/minified/formats/xhtml.js"></script>';
 
-	if($context['TPortal']['use_dragdrop'])
-		$context['html_headers'] .= '<script src="'.$boardurl.'/tp-files/tp-plugins/javascript/sceditor/minified/plugins/dragdrop.js"></script>';
+	if($context['TPortal']['use_dragdrop']) {
+		$context['html_headers'] .= '
+			<script src="'.$boardurl.'/tp-files/tp-plugins/javascript/sceditor/minified/plugins/dragdrop.js"></script>
+			<script type="text/javascript"><!-- // --><![CDATA[
+			function detectIE() {
+				var ua = window.navigator.userAgent;
+
+				// Test values; Uncomment to check result ¿
+
+				// IE 10
+				// ua = \'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)\';
+
+				// IE 11
+				// ua = \'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko\';
+
+				// Edge 12 (Spartan)
+				// ua = \'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36 Edge/12.0\';
+
+				// Edge 13
+				// ua = \'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586\';
+
+				var msie = ua.indexOf(\'MSIE \');
+				if (msie > 0) {
+					// IE 10 or older => return version number
+					return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+				}
+
+				var trident = ua.indexOf(\'Trident/\');
+				if (trident > 0) {
+					// IE 11 => return version number
+					var rv = ua.indexOf(\'rv:\');
+					return parseInt(ua.substring(rv + 3, ua.indexOf(\'.\', rv)), 10);
+				}
+
+				var edge = ua.indexOf(\'Edge/\');
+				if (edge > 0) {
+					// Edge (IE 12+) => return version number
+					return parseInt(ua.substring(edge + 5, ua.indexOf(\'.\', edge)), 10);
+				}
+
+				// other browser
+				return false;
+			}
+			// Get IE or Edge browser version
+			var version = detectIE();
+
+			if (version === false) {
+				// Do nothing
+			} else {
+				document.write(\'<script src="https://cdnjs.cloudflare.com/ajax/libs/bluebird/3.3.5/bluebird.min.js"><\/script>\');
+				document.write(\'<script src="https://cdnjs.cloudflare.com/ajax/libs/fetch/2.0.3/fetch.min.js"><\/script>\');
+			}
+			// ]]></script>';
+	}
 
 }
 
