0afc307810d485f4d6016cd1c6dbf0c938f98fe8
tinoest <tinoest@gmail.com>
2018-02-11 14:49:05 +0000

    moved progetAvatar from template to common

--- Themes/default/TPsubs.template.php
+++ Themes/default/TPsubs.template.php
@@ -83,34 +83,7 @@ function tpo_whosOnline()
 	$return = getMembersOnlineStats($membersOnlineOptions);
 	return $return;
 }
-function progetAvatars($ids)
-{
-	global $user_info, $smcFunc, $modSettings, $scripturl;
-	global $image_proxy_enabled, $image_proxy_secret, $boardurl;
-	$request = $smcFunc['db_query']('', '
-		SELECT
-			mem.real_name, mem.member_name, mem.id_member, mem.show_online,mem.avatar,
-			IFNULL(a.id_attach, 0) AS ID_ATTACH, a.filename, a.attachment_type as attachmentType
-		FROM {db_prefix}members AS mem
-		LEFT JOIN {db_prefix}attachments AS a ON (a.id_member = mem.id_member AND a.attachment_type != 3)
-		WHERE mem.id_member IN ({array_int:ids})',
-		array('ids' => $ids)
-	);
 
-	$avy = array();
-	if($smcFunc['db_num_rows']($request) > 0)
-	{
-		while ($row = $smcFunc['db_fetch_assoc']($request)) {
-			if ($image_proxy_enabled && !empty($row['id_member']) && stripos($row['avatar'], 'http://') !== false) 
-				$avy[$row['id_member']] = '<img src="'. $boardurl . '/proxy.php?request=' . urlencode($row['avatar']) . '&hash=' . md5($row['avatar'] . $image_proxy_secret) .'" alt="&nbsp;" />';
-			else
-				$avy[$row['id_member']] = $row['avatar'] == '' ? ($row['ID_ATTACH'] > 0 ? '<img ' . (in_array($row['id_member'], $user_info['buddies']) ? 'class="buddyoverlay"' : '' ). ' src="' . (empty($row['attachmentType']) ? $scripturl . '?action=dlattach;attach=' . $row['ID_ATTACH'] . ';type=avatar' : $modSettings['custom_avatar_url'] . '/' . $row['filename']) . '" alt="&nbsp;"  />' : '') : (stristr($row['avatar'], 'https://') ? '<img ' . (in_array($row['id_member'], $user_info['buddies']) ? 'class="buddyoverlay"' : '' ). ' src="' . $row['avatar'] . '" alt="&nbsp;" />' : stristr($row['avatar'], 'http://') ? '<img ' . (in_array($row['id_member'], $user_info['buddies']) ? 'class="buddyoverlay"' : '' ). ' src="' . $row['avatar'] . '" alt="&nbsp;" />' : '<img ' . (in_array($row['id_member'], $user_info['buddies']) ? 'class="buddyoverlay"' : '' ). ' src="'. $modSettings['avatar_url'] . '/' . $smcFunc['htmlspecialchars']($row['avatar']) . '" alt="&nbsp;" />');
-		}
-		$smcFunc['db_free_result']($request);
-	}
-
-	return $avy;
-}
 function TPortal_tpmodulebox($blockid)
 {
 	global $context;
