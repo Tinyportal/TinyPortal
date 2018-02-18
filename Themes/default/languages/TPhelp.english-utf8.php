e8f88903745b0d4a07b915b29f5a514594841e6d
Mick <mcjnunn@btinternet.com>
2018-02-18 13:50:52 +0000

    version update 1.4.1

--- Themes/default/languages/TPhelp.english-utf8.php
+++ Themes/default/languages/TPhelp.english-utf8.php
@@ -172,5 +172,5 @@ Thank you for efforts in making TinyPortal available around the globe.';
 $txt['tp-creditack2'] = 'Many thanks to Simple Machines Forum Team for creating such a outstanding software. Without SMF, TinyPortal would have not been possible. 
 The TinyPortal project have had much help from the following people listed below. Thanks for giving support and feedback, as well as all your valuable ideas and solutions. TP would not have been the same without you!
  Thank you Bloc for allowing the TP Team to continue your work.';
-$txt['tp-credits'] = 'TinyPortal 1.4R - Credits';
+$txt['tp-credits'] = 'TinyPortal 1.4.1 - Credits';
 ?>
