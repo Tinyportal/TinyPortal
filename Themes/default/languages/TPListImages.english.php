b37caba1606a4fe1dcb99eda2b1c00872573b700
tino <tinoest@gmail.com>
2018-10-03 21:18:15 +0100

    Moved languages back to modules directory

--- Themes/default/languages/TPListImages.english.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$txt['tp-listimage-settings'] 	= 'TinyPortal List Uploaded Images';
-$txt['tp-listimage-list'] 		= 'List Image';
-$txt['tp-listimage-remove'] 	= 'Remove Image';
-$txt['tp-listimage-username']   = 'Member';
-$txt['tp-listimage-intro']   	= 'This screen allows for listing and deleting images uploaded by users in their \'My Images Quicklist\'.';
