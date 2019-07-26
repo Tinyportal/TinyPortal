0967b3412f446f0756d169a8ad988adcd60d1042
@rjen <arjen@wellingweb.nl>
2019-07-26 15:16:18 +0200

    Fix upshrink span to li

--- Sources/TPassimilate.php
+++ Sources/TPassimilate.php
@@ -56,7 +56,7 @@ function tpAddCopy($buffer)
 
 	// add upshrink buttons
 	if( ( strpos($forum_version, '2.1') !== false ) && array_key_exists('TPortal', $context) && !empty($context['TPortal']['upshrinkpanel']) ) {
-		$buffer = preg_replace('~<div class="navigate_section">\s*<ul>~', '<div class="navigate_section"><ul><span class="tp_upshrink21">'.$context['TPortal']['upshrinkpanel'].'</span>', $buffer, 1);
+		$buffer = preg_replace('~<div class="navigate_section">\s*<ul>~', '<div class="navigate_section"><ul><li class="tp_upshrink21">'.$context['TPortal']['upshrinkpanel'].'</li>', $buffer, 1);
 	}
 	
 	// apply user membergroup colors ony when set in TP settings.
