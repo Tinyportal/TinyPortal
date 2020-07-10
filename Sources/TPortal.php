ec03850bc1e7a7d9827f2a732969fa276b6364cf
@rjen <arjen@wellingweb.nl>
2020-07-10 17:15:40 +0200

    Sorted countable issue
    2: count(): Parameter must be an array or an object that implements CountableApply Filter: Only show the errors from this fileFile: /home/camcra24/public_html/tinyportal.net/docs/Sources/TPortal.php
    Line: 2001

--- Sources/TPortal.php
+++ Sources/TPortal.php
@@ -1998,7 +1998,8 @@ function TPortal_panel($side) {{{
 			'15' => array(8, 7),
 			'16' => array(8, 8),
 		);
-		$switch = ceil(count($context['TPortal'][$panelside][$side]) / 2);
+// why is this still here? Does not seem to do anything
+//		$switch = (is_array($context['TPortal'][$panelside][$side]) ? ceil(count($context['TPortal'][$panelside][$side]) / 2) : '');
 	}
 	elseif($flow == 'horiz3') {
 		$flowgrid = array(
