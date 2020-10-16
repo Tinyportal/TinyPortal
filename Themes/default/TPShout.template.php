20a4dc7d0482a3addc8bdf14de294281c8ffc7d8
@rjen <arjen@wellingweb.nl>
2020-10-16 19:53:35 +0200

    Fix for admin setting the wysiwyg for members

--- Themes/default/TPShout.template.php
+++ Themes/default/TPShout.template.php
@@ -464,7 +464,7 @@ function template_tpshout_profile()
 		<div class="cat_bar"><h3 class="catbg">'.$txt['shoutboxprofile'].'</h3></div>
 		<p class="information">'.$txt['shoutboxprofile2'].'</p>
 		<div></div>
-		<div id="tpshout_profile" class="windowbg padding-div">
+		<div id="tpshout_profile" class="'. (!TP_SMF21 ? 'windowbg padding-div' : 'roundframe') . '">
 			<div class="windowbg addborder tp_pad">';
 	echo $txt['tp-prof_allshouts'].' <b>', $context['TPortal']['all_shouts'] ,'</b><br>';
 	echo '
