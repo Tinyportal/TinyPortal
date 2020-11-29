e4760e5b04cfb7d49e3bebdb161ee4e080d5d57a
@rjen <arjen@wellingweb.nl>
2020-11-29 23:29:51 +0100

    Fix for readmore showing even when full topic

--- TinyPortal/Article.php
+++ TinyPortal/Article.php
@@ -476,7 +476,7 @@ class Article extends Base
     }}}
 
 	public function getForumPosts( $post_ids ) {{{
-		global $context, $memberContext, $txt, $scripturl;
+		global $context, $memberContext, $txt, $scripturl, $smcFunc;
 
 		$forumPosts = $posts = array();
 		// ok we got the post ids now, fetch each one, forum first
@@ -534,10 +534,13 @@ class Article extends Base
 					$row['avatar']  = '';
 				}
 
-				if(Util::shortenString($row['body'], $context['TPortal']['frontpage_limit_len'])) {
-					$row['readmore'] = '... <p class="tp_readmore"><strong><a href="'. $scripturl. '?topic='. $row['id']. '">'. $txt['tp-readmore']. '</a></strong></p>';
+				$length = $context['TPortal']['frontpage_limit_len'];
+				if (!empty($length) && $smcFunc['strlen']($row['body']) > $length)
+				{
+					if(Util::shortenString($row['body'], $context['TPortal']['frontpage_limit_len'])) {
+						$row['readmore'] = '... <p class="tp_readmore"><strong><a href="'. $scripturl. '?topic='. $row['id']. '">'. $txt['tp-readmore']. '</a></strong></p>';
+					}
 				}
-
 				// some needed addons
 				$row['rendertype'] = 'bbc';
 				$row['frame'] = 'theme';
