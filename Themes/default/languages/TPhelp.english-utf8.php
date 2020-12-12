22d655e101d04f48041319f29f76be5fdac764d1
tino <tinoest@gmail.com>
2020-12-12 15:30:22 +0000

    Removed unnecessary duplication and included identical file

--- Themes/default/languages/TPhelp.english-utf8.php
+++ Themes/default/languages/TPhelp.english-utf8.php
@@ -1,28 +1,3 @@
 <?php
 // Version: 2.1.0; TPhelp
-
-// Credits
-$txt['tp-credit1'] = '
-<h4>Founder</h4>
-Bjorn "Bloc" Kristiansen
-<br><br>
-<h4>Developers and Contributors</h4>
-Brad "IchBin" Grow, Bloc, Illori, Arantor, tino, lurkalot, emanuele, @rjen.
-<br><br>
-<h4>TP Team</h4>
-Agridoc, Crip, Freddy, Ken, Lesmond, Renegd98, WillyP, wildenborch and ZarPrime.
-<br><br>
-<h4>TP Friends</h4>
-Aku, Anunlike, BorgBOB, deniz, Dragooon, eeek, eld^kar, feline, Fussilet, G6, gerrymo, Ianedres, JPDeni, Max, Maya, Moocat, nokonium, PhilH, Rasyr, Ray, RebelRose, redone, StormLrd, Thurnok, Tuxie, Xarcell, and Zetan.
-<br><br>
-<h4>Beta Testers</h4>
-For those who relentlessy find bugs, give feedback, and drive the developer crazy.
-<br><br>
-<h4>Language Translators</h4>
-Thank you for efforts in making TinyPortal available around the globe.';
-
-$txt['tp-creditack2'] = 'Many thanks to Simple Machines Forum Team for creating such a outstanding software. Without SMF, TinyPortal would have not been possible.
-The TinyPortal project have had much help from the following people listed below. Thanks for giving support and feedback, as well as all your valuable ideas and solutions. TP would not have been the same without you!
- Thank you Bloc for allowing the TP Team to continue your work.';
-$txt['tp-credits'] = 'TinyPortal - Credits';
-?>
+require_once(LANGUAGEDIR.'/TPhelp.english.php');
