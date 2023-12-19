0dbb3b05c015b83b06dbbe718e738c473a014675
tino <tinoest@gmail.com>
2023-12-19 22:15:14 +0000

    More of the same to include the init

--- Sources/TPShout.php
+++ Sources/TPShout.php
@@ -860,7 +860,14 @@ function TPShoutAdmin() {{{
 		$tpstart = 0;
     }
 
-	require_once($sourcedir . '/Subs-Post.php');
+	if(TP_SMF21) {
+		require_once($sourcedir . '/Subs-Post.php');
+	}
+	else {
+		// We don't require it until we load now so need to load seperately...
+		require_once($sourcedir.'/TPortal.php');
+		TPortal_init();
+	}
 	loadtemplate('TPShout');
 
 	$context['template_layers'][] = 'tpadm';
